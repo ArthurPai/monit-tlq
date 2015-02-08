1016cae8cbcac92f130a5eee0ffccbfaf2073aad
ArthurPai <baidragoon@gmail.com>
2015-02-08 17:39:02 +0800

    update version

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Arthur Pai"
 maintainer_email  "baidragoon@gmail.com"
 description       "Basic monit installation"
-version           "0.3.10"
+version           "0.3.11"
 
 supports      "ubuntu"
